49809c696c751d97b599b2484e3fe33f42127ca5
changwoo <ep6tri@hotmail.com>
2025-05-05 00:22:30 +0900

    중복된 코드 삭제

--- inc/templates/admin-meta-level.tmpl.php
+++ inc/templates/admin-meta-level.tmpl.php
@@ -3,8 +3,8 @@
  * @var Template $this
  *
  * Context:
- * WP_Term[]  $levels
- * int[]      $values
+ * - levels: WP_Term[]
+ * - values: int[]
  */
 
 use Bojaghi\Template\Template;
@@ -13,23 +13,3 @@ if (!defined('ABSPATH')) {
     exit;
 }
 ?>
-
-<ul>
-    <?php
-    foreach ($this->get('levels') as $level) :
-        /** @var WP_Term $level */
-        ?>
-        <li>
-            <input
-                id="<?php echo esc_attr(KMC_TAX_LEVEL . '-' . $level->term_id) ?>"
-                name="<?php echo esc_attr(KMC_TAX_LEVEL) ?>[]"
-                type="checkbox"
-                value="<?php echo esc_attr($level->term_id) ?>"
-                <?php checked(in_array($level->term_id, $this->get('values', []), true)); ?>
-            />
-            <label for="<?php echo esc_attr(KMC_TAX_LEVEL . '-' . $level->term_id) ?>">
-                <?php printf('%s', esc_html($level->name)); ?>
-            </label>
-        </li>
-    <?php endforeach; ?>
-</ul>
